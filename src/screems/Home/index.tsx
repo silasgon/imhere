81c199709f0342a1affb2f6fdecfe2d265f7dd6f
Natan <natangon@gmail.com>
2023-08-30 23:41:47 -0300

    Criando o form

--- src/screems/Home/index.tsx
+++ src/screems/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+
+  function handleParticipantAdd(){
+    console.log("Você clicou no botão de Adicionar!")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -12,12 +17,16 @@ export function Home() {
       <Text style={styles.eventDate}>
         Sexta, 4 de Novembrod de 2022
       </Text>
-      <TextInput 
-      style={styles.input}
-      placeholder="Nome do Participante"
-      placeholderTextColor="#6b6b6b"
-      
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do Participante"
+        placeholderTextColor="#6b6b6b"
       />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 
--- src/screems/Home/styles.ts
+++ src/screems/Home/styles.ts
@@ -23,5 +23,17 @@ export const styles = StyleSheet.create({
         color: '#fff',
         padding: 16,
         fontSize: 16
+    },
+    buttonText:{
+        color: '#fff',
+        fontSize: 24,
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius:5,
+        backgroundColor: '#31CF67',
+        alignItems: "center",
+        justifyContent: 'center',
     }
 });
