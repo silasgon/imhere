7b01658620743c6f790a8d96c25946df5080924d
Natan <natangon@gmail.com>
2023-09-04 00:32:57 -0300

    Componentização e make component Participant

--- src/screems/Home/index.tsx
+++ src/screems/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
+import { Participant } from "../../components/Participant";
+
 import { styles } from './styles';
 
 export function Home() {
@@ -29,6 +31,10 @@ export function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+
+      <Participant/>
+      <Participant/>
+      <Participant/>
     </View>
   );
 
