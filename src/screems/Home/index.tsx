799fc550afe4aa3c3f758f8906903959e49744fa
Natan <natangon@gmail.com>
2023-09-09 22:46:38 -0300

    removendo participant

--- src/screems/Home/index.tsx
+++ src/screems/Home/index.tsx
@@ -20,10 +20,11 @@ export function Home() {
   }
 
   function handleParticipantRemove(name: string) {
+    
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert("Deletado")
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: 'Não',
